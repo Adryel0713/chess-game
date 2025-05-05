63d792a2a053ea57d45f60e13f1fb8b41f566474
Adryel0713 <clauber.adriel@gmail.com>
2025-05-05 14:48:41 -0300

    First class: Position

--- src/br/nullPointerX/Program.java
+++ src/br/nullPointerX/Program.java
@@ -1,7 +1,10 @@
 package br.nullPointerX;
 
+import br.nullPointerX.boardGame.Position;
+
 public class Program {
     public static void main(String[] args) {
-
+        Position p = new Position(3,4);
+        System.out.println(p);
     }
 }
